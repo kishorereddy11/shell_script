fa58082c61cabd8371eba67104e4eb12751940a6
kishor <kishorebsc12@gmail.com>
2024-12-31 12:58:40 +0530

    shell script

---
+++ special-char.sh
@@ -0,0 +1,13 @@
+write shell script to print special characters in shell script
+
+echo "Special characters in shell script are :"
+echo "all variable are passed: $@ "
+echo "all variable are passed: $* "
+echo "all variable are passed: $# "
+echo "script name: $0 "
+echo "last command status: $? "
+echo "last command status: $$ "
+echo "last command status: $! "
+echo "last command status: $- "
+echo "last command status: $* "
+echo "home directory: $HOME "
