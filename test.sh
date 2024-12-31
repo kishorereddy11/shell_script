cec7ebfe585311201b5b11ad4d5c93ddfefd396a
kishor <kishorebsc12@gmail.com>
2024-12-31 12:44:45 +0530

    shell script

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ echo "Hello World parameter1 : $1"
 
 echo "Present working directory : $(pwd)"
 
-echo "List of files in current directory : $(ls)"
-
 echo "List of files in current directory : $(ls -l)"
+
+echo "List of files in current directory : $(ls -l | awk '{print $9}')"
 
