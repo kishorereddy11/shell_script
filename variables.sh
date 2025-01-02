7c80aa22d25cd85f7552d59100c6a0b5a32ba7fd
kishor <kishorebsc12@gmail.com>
2025-01-02 17:56:28 +0530

    functions added

--- variables.sh
+++ variables.sh
@@ -3,6 +3,7 @@ echo "All variables passed: $@"
 echo "Number of variables passed: $#"
 echo "Current user $USER .... $(USER)"
 echo "process id of current script $$"
+sleep 60 &
 echo "process id last command in background $!"
 echo "script name $0"
 echo "present working directory $PWD"
