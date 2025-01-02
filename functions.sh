8f6272ffa3c849cf400b64221eeefc75a92e4d75
kishor <kishorebsc12@gmail.com>
2025-01-02 18:37:26 +0530

    functions added

--- functions.sh
+++ functions.sh
@@ -5,8 +5,11 @@ if [ $USER_ID -eq 0 ]
     then 
     echo "cuurent user is root"
     else
-    echo "Not a Root user"
+    echo "Not a Root user .. you must login root user to execute this script"
     exit 1
 fi
 
+dnf install mysql -y
+
+dnf install git -y 
 #for package in 
