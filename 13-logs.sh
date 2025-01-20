48934396a9abc54be4fdeddffc87cc20b5c65139
kishor <kishorebsc12@gmail.com>
2025-01-20 17:59:20 +0530

    example

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 VALIDATE() {
 
 if [ $? -ne 0 ]
@@ -14,27 +19,29 @@ echo -e "$2 $G Success"
 fi 
 }
 
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
 if [ $USER_ID -ne 0 ]
 then 
 echo "ERROR: you must have sudo access to execute this script"
 exit 1
 fi
 
-dnf list installed mysql 
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then 
-dnf install mysql -y
+dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MYSQL"
 else
 echo -e "Mysql is alredy... $Y installed"
 fi
 
-dnf list installed git
+dnf list installed git &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
-dnf install git -y
+dnf install git -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing git"
 else
 echo -e "Git is already... $Y installed"
