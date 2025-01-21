40f2a0bd09c10a1eb7ba54e64ef748792bb09478
kishor <kishorebsc12@gmail.com>
2025-01-21 11:42:29 +0530

    example

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -69,3 +69,9 @@ done
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "FILES to be deleted $FILES_TO_DELETE"
 
+while read -r file
+
+do
+echo "Delete file: $file"
+
+done <<< $FILES_TO_DELETE
